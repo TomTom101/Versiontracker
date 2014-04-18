96f144510b5e04c402c1aad93fc14460f344e574
Thomas Brandl <thobra@gmail.com>
2014-04-18 17:39:31 +0200

    Added simple static file server to serve HTML file. Needs node-static

--- www/server.js
+++ www/server.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 
-var sys = require("sys"),  
+var sys = require("sys"),
  http = require("http"),
  url = require("url"),
  cache = require('memory-cache'),
- auth_config = require('./auth.js');
+ auth_config = require('./auth.js'),
+ static = require('node-static');
 
 proxyRequest = function(url, r) {
 	if(resp = cache.get(url)) {
-		sys.puts("From cache"); 
+		sys.puts("From cache");
 		r.setHeader("Access-Control-Allow-Origin", "*");
 		r.write(resp)
-		r.end(); 
+		r.end();
 		return
 	}
 
-	sys.puts("Calling "  + url);   
+	sys.puts("Calling "  + url);
 		var request = require("request")
 		var resp = ""
 		request(url, function (error, response, body) {
@@ -27,15 +28,33 @@ proxyRequest = function(url, r) {
 			}
 			r.setHeader("Access-Control-Allow-Origin", "*")
 			r.write(cache.get(url))
-			r.end(); 
+			r.end();
 		}).auth(auth_config.user, auth_config.pass, auth_config.sendImmediately);
 }
-http.createServer(function(request,response){  
+http.createServer(function(request,response){
 	var url_parts = url.parse(request.url, true);
-    //response.writeHeader(200, {"Content-Type": "text/json"}); 
+    //response.writeHeader(200, {"Content-Type": "text/json"});
     if(url_parts.query.url) {
     	proxyRequest(url_parts.query.url, response)
     }
-     
-}).listen(8081);  
-sys.puts("Server Running on 8081");   
+
+}).listen(8081);
+
+sys.puts("Proxy running on 8081");
+
+var file = new static.Server('.');
+
+http.createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response, function(err, result) {
+        	if (err) { // There was an error serving the file
+                sys.error("Error serving " + request.url + " - " + err.message);
+
+                // Respond to the client
+                response.writeHead(err.status, err.headers);
+                response.end();
+            }
+        });
+    }).resume();
+}).listen(8080);
+sys.puts("Server running on 8080");
